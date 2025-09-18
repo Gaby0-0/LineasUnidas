8077535aa08c0984ed1b02a758ef7bf863ad0c41
Gaby0-0 <gabriela0203martinez@gmail.com>
2025-09-18 14:07:19 -0600

    Segundo push de inicio y enlaces

--- src/main/java/LineasUnidas/demo/controllers/RegistroClienteController.java
+++ src/main/java/LineasUnidas/demo/controllers/RegistroClienteController.java
@@ -4,6 +4,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import LineasUnidas.demo.model.Cliente;
@@ -27,8 +28,29 @@ public class RegistroClienteController {
 
     @PostMapping("/registro-cliente")
     public String registrarCliente(@ModelAttribute Cliente cliente, RedirectAttributes redirectAttrs) {
-        clienteService.registrarCliente(cliente); // guarda usando tu service
+        clienteService.registrarCliente(cliente); 
         redirectAttrs.addFlashAttribute("mensaje", "Registro exitoso!");
-        return "redirect:/login"; // redirige al login
+        return "redirect:/login"; 
+    }
+
+
+        @PostMapping("/login")
+    public String loginCliente(
+            @RequestParam("correo") String correo,
+            @RequestParam("contrasena") String contrasena,
+            Model model) {
+
+        // Buscar el cliente por correo (o nombre de usuario)
+        Cliente cliente = clienteService.obtenerClientePorCorreo(correo);
+
+        if (cliente != null && cliente.getContrasenia().equals(contrasena)) {
+            // Login correcto
+            model.addAttribute("cliente", cliente);
+            return "redirect:/inicio-gerente"; // Página de bienvenida
+        } else {
+            // Login fallido
+            model.addAttribute("error", "Usuario o contraseña incorrectos");
+            return "login"; // Retorna a la página de login
+        }
     }
 }
